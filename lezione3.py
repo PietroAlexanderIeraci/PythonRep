da96681f0b7ee106a7be9f247f4c19cd7873a287
Pietro Alexander Ieraci <115617834+PietroAlexanderIeraci@users.noreply.github.com>
2022-12-12 18:46:36 +0000

    added lezione 3

---
+++ lezione3.py
@@ -0,0 +1,15 @@
+def sum_csv(file):
+    x = open(file)
+    l = []
+    for line in x:
+        elements = line.split (",")
+        l.append(elements[1])
+    x = 0
+    for n in l[1:]:
+        try:
+            x += float(n)
+        except ValueError:
+            continue
+    if x == 0:
+        return None
+    return x
